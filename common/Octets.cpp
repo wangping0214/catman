d7ca544f8cf8a734ac77346441122efe4c442e8d
Alanmars <wangping0214@126.com>
2013-03-04 18:52:33 +0800

    Define the interface of Octets; implement the Octets::Rep.

--- common/Octets.cpp
+++ common/Octets.cpp
@@ -1,1 +1,95 @@
 #include "Octets.cpp"
+#include <stdlib.h>
+
+namespace catman
+{
+namespace common
+{
+
+Octets::Rep Octets::Rep::null;
+
+Octets::Rep::Rep() : m_capacity(0), m_size(0), m_ref(0)
+{
+}
+
+void Octets::Rep::incRef()
+{
+	++ m_ref;
+}
+
+void Octets::Rep::decRef()
+{
+	if (-- m_ref == 0)
+		delete this;
+}
+
+void* Octets::Rep::data()
+{
+	return reinterpret_cast<void*>(this + 1);
+}
+
+void* Octets::Rep::unique()
+{
+	if (m_ref > 1)
+	{
+		Rep *rep = clone();
+		decRef();
+		return rep->data();
+	}
+	return data();
+}
+
+void* Octets::Rep::reserve(size_t size)
+{
+	size = alignSize(size);
+	if (size > m_capacity)
+	{
+		Rep *rep = create(size);
+		memcpy(rep->data(), data(), m_size);
+		rep->m_size = m_size;
+		return rep->data();
+	}
+	return unique();
+}
+
+Rep* Octets::Rep::create(size_t capacity)
+{
+	Rep *rep = new (capacity) Rep;
+	rep->m_capacity = capacity;
+	rep->m_size = 0;
+	rep->m_ref = 1;
+	return rep;
+}
+
+Rep* Octets::Rep::clone()
+{
+	Rep *rep = create(m_capacity);
+	memcpy(rep->data(), data(), m_size);
+	rep->m_size = m_size;
+	return rep;
+}
+
+size_t Octets::Rep::alignSize(size_t size)
+{
+	size_t tmp = 16;
+	while (size > tmp)
+		tmp <<= 1;
+	return tmp;
+}
+
+void* Octets::Rep::operator new(size_t size, size_t extra)
+{
+	return malloc(size + extra);
+}
+
+void* Octets::Rep::operator delete(void *p)
+{
+	free(p);
+}
+
+////////////////////////////////////////////////////////////////
+
+Octets::Octets() : 
+
+}
+}
--- common/Octets.h
+++ common/Octets.h
@@ -8,11 +8,56 @@ namespace common
 
 class Octets
 {
+	class Rep
+	{
+	public:
+		Rep();
+		void incRef();
+		void decRef();
+		void* data();
+		void* unique();
+		void* reserve(size_t size);
+		static Rep* create(size_t capacity);
+	private:
+		Rep* clone();
+		static size_t alignSize(size_t size);
+		static void* operator new(size_t size, size_t extra);
+		static void* operator delete(void *p);
+	public:
+		size_t m_capacity;
+		size_t m_size;
+		size_t m_ref;
+		
+		static Rep null;
+	};
 public:
 	Octets();
 	Octets(size_t size);
-	~Octets();
+	Octets(const void *buff, size_t len);
+	Octets(const void *beginPos, const void *endPos);
+	Octets(const Octets &other);
+	virtual ~Octets();
+	Octets& operator = (const Octets &other);
+	bool operator == (const Octets &other);
+	bool operator != (const Octets &other);
+	Octets& swap(Octets &other);
+	void* begin();
+	void* end();
+	const void* begin() const;
+	const void* end() const;
+	size_t size() const;
+	size_t capacity() const;
+	Octets& clear();
+	Octets& erase(size_t pos, size_t len);
+	Octets& erase(void *beginPos, void *endPos);
+	Octets& insert(void *pos, const void *buff, size_t len);
+	Octets& insert(void *pos, const void *beginPos, const void *endPos);
+	Octets& resize(size_t size);
 private:
+	Rep *rep() const;
+	void unique();
+private:
+	void *m_base;
 };
 
 }
